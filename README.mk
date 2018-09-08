39d5ac3b663305f9bfdfbc67ce9fd21c52ff503f
Fred Khesia <you@example.com>
2018-09-08 12:28:06 +0300

    edited README

---
+++ README.mk
@@ -0,0 +1,11 @@
+# {PERSONAL WEBSITE}
+#### {THIS IS MY OFFICIAL WEBSITE}, {08.09.2014}
+#### By **{FRED KHEISA ,GEEROCK TRICKS, JOY}**
+## THIS IS A WEBSITE YOU WILL GET EVERYTHING YOU NEED TO KNOW ABOUT ME AND THE PROJECTS I HAVE COVERED AS A DEVELOPER}
+## made for 
+* @media screen and (min-width: 480px) and above meaning that the devices as small as phones and tablets to computers can access it
+## Known Bugs {there are potential bugs but that can be resoved and the app being improved }
+## Technologies Used { the tecnologies used are both css and html }
+## Support and contact details {in case of any problems and bug be sure to contact at +254702049058}
+### License 
+*{MIT LICENCE see below for more details } *Copyright (c) {2018} **{KYSER ENTERPRISE}**
